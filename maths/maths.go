c2c6ecd0a413be5f3aec1519f5db31626ed2ac95
marcelhuang(黄保福) <marcelhuang@tencent.com>
2021-06-17 18:06:32 +0800

    新增运算类型

--- maths/maths.go
+++ maths/maths.go
@@ -1,7 +1,29 @@
 package maths
-func Add(num1,num2 int)int{
-	return num1 + num2
+
+import "errors"
+//加法
+func Add(num1, num2 int) (int, error) {
+	return num1 + num2, nil
 }
-func Sub(num1,num2 int)int{
-return num1 - num2
+//减法
+func Sub(num1, num2 int) (int, error) {
+	return num1 + num2, nil
+}
+
+//乘法
+func Mutil(num1, num2 float32) (float32, error) {
+	return num1 + num2, nil
+}
+
+//乘法
+func Div(num1, num2 float32) (float32, error) {
+	if num2 == 0 {
+		return 0, errors.New("被除数为0！")
+	}
+	return num1 / num2, nil
+}
+
+//模运算
+func Mod(num1, num2 int) (int, error) {
+	return num1 % num2, nil
 }
