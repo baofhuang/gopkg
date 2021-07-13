42f3fae0091ac480d3277e41f009d4683b311e81
marclehuang <1174421473@qq.com>
2021-07-13 17:58:16 +0800

    add readCSV

---
+++ readCSV/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main()  {
+	//准备读取文件
+	fileName := "./testdata/acs_role_membername.csv"
+	fs, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("can not open the file, err is %+v", err)
+	}
+	defer fs.Close()
+	r := csv.NewReader(fs)
+	//针对大文件，一行一行的读取文件
+	for {
+		row, err := r.Read()
+		if err != nil && err != io.EOF {
+			log.Fatalf("can not read, err is %+v", err)
+		}
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(row)
+	}
+}
